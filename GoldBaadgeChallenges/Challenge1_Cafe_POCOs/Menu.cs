62f2d2b4c330dfce74cb11a177d3bdbd0d3b334d
John Peters <petersjohn0109@gmail.com>
2021-07-08 19:40:35 -0400

    Completed the POCO and REPO

---
+++ GoldBaadgeChallenges/Challenge1_Cafe_POCOs/Menu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1_Cafe_POCO
+{
+    
+    public class MenuContent
+    {
+        public MenuContent() { }
+
+        public MenuContent(string itemName, int itemNumber, string itemDescription, decimal itemPrice, IList<string> itemIngredients)
+        {
+            ItemName = itemName;
+            ItemNumber = itemNumber;
+            ItemDescription = itemDescription;
+            ItemPrice = itemPrice;
+            ItemIngredients = itemIngredients;
+         }
+
+        public string ItemName { get; set; }
+        public int ItemNumber { get; set; }
+
+        public string ItemDescription { get; set; }
+
+        public decimal ItemPrice { get; set; }
+
+        public IList<string> ItemIngredients { get; set; }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ GoldBaadgeChallenges/Challenge1_Cafe_Repo/MenuRepo.cs
@@ -0,0 +1,85 @@
+﻿using Challenge1_Cafe_POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1_Cafe_REPO
+{
+    public class MenuRepo
+    {
+        //CRUD
+
+        private readonly List<MenuContent> _menuContent = new List<MenuContent>();
+
+        //create
+
+        public bool AddItemsToMenu(MenuContent content)
+        {
+            int menuItemCount = _menuContent.Count();
+            _menuContent.Add(content);
+            if(_menuContent.Count() == menuItemCount + 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //Read
+
+        public List<MenuContent> GetMenu()
+        {
+            return _menuContent;
+        }
+        
+        //UPDATE
+        //Not implementing Updates at this time
+
+        //Delete
+
+        public bool DeleteItemByNumber(int menuNumber)
+        {
+            MenuContent content = GetMenuContentByItemNumber(menuNumber);
+            if(content == null)
+            {
+                return false;
+            }
+
+            int sizeOfMenuBefore = _menuContent.Count;
+            _menuContent.Remove(content);
+            if(sizeOfMenuBefore > _menuContent.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            
+
+        }
+
+        public MenuContent GetMenuContentByItemNumber(int menuNumber)
+        {
+            
+            foreach(var item in _menuContent)
+            {
+                if(item.ItemNumber == menuNumber)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        
+
+
+
+        
+
+       
+    }
+}
